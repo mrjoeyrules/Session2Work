aa54634859d438da5383c5d44fb9839ca3475a18
mrjoeyrules <mrjoeyrules@gmail.com>
2022-02-07 18:34:37 +0000

    Exercose 3

--- Session2Work/Session2Work.cpp
+++ Session2Work/Session2Work.cpp
@@ -31,10 +31,41 @@ void exercise2()
 	}
 }
 
-
+void exercise3()
+{
+	int a = 2;
+	int b = 4;
+	int c = 6;
+	int d = 8;
+	if (a == 2 && b < 3)
+	{
+		cout << "Test A";
+	}
+	else if (a != 3 && b > 2)
+	{
+		cout << "Test B";
+	}
+	else if (c < 7 || d > 9)
+	{
+		cout << "Test C";
+	}
+	else if (c >= 6 || d <= 10)
+	{
+		cout << "Test D";
+	}
+	else if (a == 2 && d > 7)
+	{
+		cout << "Test E";
+	}
+	else
+	{
+		cout << "None are met";
+	}
+}
 
 int main()
 {
 	//exercise1();
 	//exercise2();
+	exercise3();
 }
