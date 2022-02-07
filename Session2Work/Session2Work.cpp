04abee79c235dcb05b34c0daa9ccfc7de756bb52
mrjoeyrules <mrjoeyrules@gmail.com>
2022-02-07 18:05:12 +0000

    exercise1 Complete

--- Session2Work/Session2Work.cpp
+++ Session2Work/Session2Work.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
+
+void exercise1()
+{
+    int aInt = 10;
+	if (aInt <= 9)
+	{
+		cout << "Number is too low";
+	}
+	else if (aInt >= 11)
+	{
+		cout << "Number is too high";
+	}
+	else
+	{
+		cout << "Number is perfect";
+	}
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+	exercise1();
 }
